f2a022c86a6b4c3a9ff06a6f78e5c02c4dbbe9a8
dueda <bnyt_zhangzhaojun@163.com>
2021-05-28 10:01:00 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by dueda on 2021/5/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
